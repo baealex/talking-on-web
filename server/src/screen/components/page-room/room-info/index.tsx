af2dedbd614eb2435f5023bacaec675afd5b0b3b
baealex <im@baejino.com>
2021-08-04 22:35:13 +0900

    ⭐ Redesign room info

--- server/src/screen/components/page-room/room-info/index.tsx
+++ server/src/screen/components/page-room/room-info/index.tsx
@@ -21,16 +21,16 @@ export function RoomInfo(props: RoomInfoProps) {
             <div className={cn('box')}>
                 <div className="container">
                     <div className={cn('nav')}>
-                        <Link href="/">
-                            <a>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" display="block" id="ChevronLeft"><path d="M15 4l-8 8 8 8"/></svg>
-                            </a>
-                        </Link>
                         <div>
                             <a onClick={() => setIsOpen((prevState) => !prevState)}>
                                 {props.users.length}/12
                             </a>
                         </div>
+                        <Link href="/">
+                            <a>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" display="block" id="Door"><path d="M5 2h11a3 3 0 0 1 3 3v14a1 1 0 0 1-1 1h-3"/><path d="M5 2l7.588 1.518A3 3 0 0 1 15 6.459V20.78a1 1 0 0 1-1.196.98l-7.196-1.438A2 2 0 0 1 5 18.36V2z"/><path d="M12 12v2"/></svg>
+                            </a>
+                        </Link>
                     </div>
                 </div>
             </div>
